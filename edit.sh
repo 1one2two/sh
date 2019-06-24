4be410628313b0ad10a23193667fab0e74871d32
motebus <info@ypcloud.com>
2019-06-24 14:42:17 +0800

    first commit

---
+++ edit.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-flow.sh'
+sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-io.sh'
+sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-main.sh'
+
+sed -i 's/"DCenter":"dc@dc.ypcloud.com:6789",/"DCenter":"dc@127.0.0.1",/g' '/home/pi/echo_ei-db/conf/config.json'
+sed -i 's/"DCenter":"dc@dc.ypcloud.com:6789",/"DCenter":"dc@127.0.0.1",/g' '/home/pi/echo_ei-mms/conf/config.json'
