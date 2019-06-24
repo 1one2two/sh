c2e410b380fdc98693ae3b7e9fadb23979ad8fe3
1one2two <ej04vm6@gmail.com>
2019-06-24 15:23:32 +0800

    Update edit.sh

--- edit.sh
+++ edit.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-flow.sh'
-sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-io.sh'
-sed -i 's/DC=.................../DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-main.sh'
+sed -i 's/DC="dc@dc.ypcloud.com"/DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-flow.sh'
+sed -i 's/DC="dc@dc.ypcloud.com"/DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-io.sh'
+sed -i 's/DC="dc@dc.ypcloud.com"/DC="dc@127.0.0.1"/g' '/home/pi/anko/rtu-main.sh'
 
 sed -i 's/"DCenter":"dc@dc.ypcloud.com",/"DCenter":"dc@127.0.0.1",/g' '/home/pi/echo_ei-db/conf/config.json'
 sed -i 's/"DCenter":"dc@dc.ypcloud.com",/"DCenter":"dc@127.0.0.1",/g' '/home/pi/echo_ei-mms/conf/config.json'
